e746e2ff2daf7ba2ca02099e1eb79ea0a869504b
Alicja Tomaszewska <tomaszewskaalicja93@gmail.com>
2023-11-27 19:18:43 +0100

    Ala

--- dummy.py
+++ dummy.py
@@ -1,8 +1,3 @@
 new_string = "bambor"
-<<<<<<< Updated upstream
-new_string3 = "pianka"
-new_string4 = "mam_kalkulator"
-
-=======
 new_string2 = "bambik"
->>>>>>> Stashed changes
+new_string10 = "ala"
